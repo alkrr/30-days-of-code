41a2166e9323c6b3fe69112a5d06b3ce8004b640
alkrr <alkrr@outlook.com>
2016-07-06 10:41:04 -0700

    1 + 2

---
+++ day 1/hello.py
@@ -0,0 +1,10 @@
+"""
+To complete this challenge, you must save a line of input from stdin to a variable, 
+print Hello, World. on a single line, and finally print the value of your variable on a second line.
+"""
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+inputString = raw_input()
+
+# Print a string literal saying "Hello, World."
+print 'Hello, World.'
---
+++ day 2/data_types.py
@@ -0,0 +1,24 @@
+"""
+Task 
+Complete the code in the editor below. The variables i, d, and s are 
+already declared and initialized for you. You must declare 3 variables: 
+one of type int, one of type double, and one of type String. 
+Then you must read 3 lines of input from stdin and initialize your  variables. 
+Finally, you must use the  operator to perform the following operations:
+
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line.
+"""
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+i2 = int(raw_input())
+d2 = float(raw_input())
+s2 = raw_input()
+
+print i + i2
+print d + d2
+print s + s2
